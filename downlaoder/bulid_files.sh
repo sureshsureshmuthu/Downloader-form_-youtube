429bd8f99b9b64f38931d371db46819948fd583b
sureshsureshmuthu <165756771+sureshsureshmuthu@users.noreply.github.com>
2024-07-12 11:55:39 +0530

    Update bulid_files.sh

--- downlaoder/bulid_files.sh
+++ downlaoder/bulid_files.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Install dependencies
 pip install -r requirements.txt
-python3.11.4 manage.py collectstatic
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Create a directory for the static files
+mkdir -p staticfiles_build
+
+# Move the collected static files to the staticfiles_build directory
+mv static staticfiles_build/
